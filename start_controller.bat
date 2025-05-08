b52f2e0df4023ce48ed41c4b6b570a34d946f023
James Philip <88029424+JamesVSeVERYBODY@users.noreply.github.com>
2025-05-08 12:31:40 +0700

    adding bat file to start every programs that needed

---
+++ start_controller.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Starting Steering System...
+
+REM Start Python script (PERINTAH, bukan file)
+cd "C:\Users\cuzt2\OneDrive\Desktop\KULIAH\TUGAS AKHIR\PRATA\Steering Controls"
+start "" python main.py
+
+REM Start 360ce
+start "" "C:\Games\Need for Speed - Most Wanted\x360ce.exe"
+
+exit
